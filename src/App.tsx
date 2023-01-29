596bc2ee3db21215685dabcfb5f964a31b9c57d8
riyadism <riyad.rir@gmail.com>
2023-01-29 21:51:04 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import { Refine } from "@pankod/refine-core";
+import {
+  notificationProvider,
+  RefineSnackbarProvider,
+  CssBaseline,
+  GlobalStyles,
+  Layout,
+  ThemeProvider,
+  LightTheme,
+  ReadyPage,
+  ErrorComponent,
+} from "@pankod/refine-mui";
+
+import dataProvider from "@pankod/refine-simple-rest";
+import routerProvider from "@pankod/refine-react-router-v6";
+import { MuiInferencer } from "@pankod/refine-inferencer/mui";
+
+
+function App() {
+  return (
+    <ThemeProvider theme={LightTheme}>
+      <CssBaseline />
+      <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
+      <RefineSnackbarProvider>
+        <Refine
+          dataProvider={dataProvider("https://api.fake-rest.refine.dev")}
+          notificationProvider={notificationProvider}
+          Layout={Layout}
+          ReadyPage={ReadyPage}
+          catchAll={<ErrorComponent />}
+          routerProvider={routerProvider}
+          resources={[
+            {
+                name: "products",
+                list: MuiInferencer,
+                show: MuiInferencer,
+                create: MuiInferencer,
+                edit: MuiInferencer,
+            },
+        ]}
+        />
+      </RefineSnackbarProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
